0c4be300d1c890aaa239be6c26ffa4848d32afaf
kotek <scpktk@gmail.com>
2026-03-18 19:22:11 +0900

    fix

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer class="flex flex-col items-center bg-black text-white py-12">
+    <footer class="flex flex-col items-center bg-black text-white pb-20 pt-4">
       <img src="/images/doshisha-robot-logo.svg" alt="Doshisha Robot Logo" class="h-48 w-auto" />
       <p class="text-gray-400 font-[Jost]">&copy; Doshisha Robotics Club</p>
     </footer>
